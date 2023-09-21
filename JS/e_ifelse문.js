83b80c67f69a1493374484ac98db2bcc5dfd06ac
Young-Jun-Park18 <112959373+Young-Jun-Park18@users.noreply.github.com>
2023-09-22 01:56:40 +0900

    5.ifelse

---
+++ JS/e_ifelse문.js
@@ -0,0 +1,13 @@
+//비교연산자
+console.log(10>=5);
+console.log("1"==1); //동등연산자
+console.log("1"===1); //일치연산자 (타입까지 비교한다)
+console.log(10!=5);
+console.log(10<5);
+
+
+//조건문 if - else - else if는 파이썬과 같다
+
+//논리연산자 &&(and) ||(or) !(not)
+
+//반복문 c언어와 동일하다 ex) for(i = 0; i < 10; i++)
